544fbbf3ab242ba12793aa7563a02c4c90cfb12e
Abhishek1380 <undeabhishek707@gmail.com>
2024-02-17 09:59:53 +0530

    Hashing Passwords using bcryptjs

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,12 @@ app.use(express.json());
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'));
+
 // app.use('/api/notes', require('./routes/notes'))
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("Hello Worl");
 })
 
 // app.listen(port, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User1.js');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = "Imgonnabegreat";
 // router.get('/', (req, res) => {
 //     console.log(req.body);
 //     res.send("Hello alina rai");
@@ -29,15 +32,39 @@ router.post('/createUser', [
             return res.status(400).json({ error: "User with this email already exists" });
         }
 
+
+
+
+
+
+        // Here after creating user password will directlt be stored in database. We don't want that. so instead we would add salt in password using bcrypt
+        // user = await User.create({
+        //     name: req.body.name,
+        //     password: req.body.password,
+        //     email: req.body.email
+        // });
         // Create a new user
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email
         });
 
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        console.log(authToken);
+
+
+
         // Send the created user object in response
-        res.status(201).json(user);
+        res.status(201).json(authToken);
     } catch (error) {
         console.error('Error creating user:', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
