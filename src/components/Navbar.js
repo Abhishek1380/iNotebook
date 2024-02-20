fc50930d634e3adb5effe22e50d4e2107230a908
Abhishek1380 <undeabhishek707@gmail.com>
2024-02-20 22:47:09 +0530

    component files added

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div>
+            This is navbar
+        </div>
+    )
+}
+
+export default Navbar
