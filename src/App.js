39d08c2a5c2c92d7aab2e5373cd15ce61ad0e732
Abhishek1380 <undeabhishek707@gmail.com>
2024-02-27 21:20:54 +0530

    Created Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';   By default when react is installed
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from abhishek</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        {/* 
+          <Route path="/users" element={<Users />} />
+        */}
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
